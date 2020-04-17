f124e499ed6fdbd4a2cc3fdf6dccca0caf5f71cd
avvasai <avva.saipranav@gmail.com>
2020-04-18 02:18:55 +0530

    Testing git through emacs

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#A comment, tis is so you can tead your program later.
+# Anything after the # is ifnored by python.
+
+print "I could have code like this" #and the comment is ignored
+
+# you can also use a comment "disable" or commnet out a piece of cod:
+# print  "This wont run."
+
+print "This will run"
